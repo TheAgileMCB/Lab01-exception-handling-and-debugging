9647078a48b80e24a78ca29d3b7c122d97bc27c4
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-04 17:15:53 -0500

    added initial functions requested

---
+++ Lab01-ExceptionHandlingAndDebugging/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Lab01_ExceptionHandlingAndDebugging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StartSequence();
+                Console.WriteLine();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Mistakes were made. Whoops.");
+                throw;
+            }
+            finally
+            {
+                Console.WriteLine("Program COMPLETE!");
+            }
+        }
+
+        static void StartSequence()
+        {
+
+        }
+
+        static void Populate()
+        {
+
+        }
+
+        static int GetSum()
+        {
+
+        }
+
+        static int GetProduct()
+        {
+
+        }
+
+        static int GetQuotient()
+        {
+
+        }
+    }
+}
